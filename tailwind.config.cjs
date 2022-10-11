8d8040e2b09e2148b3bfaca09b96b47948e38172
Santiago Toyos <ppikachu@gmail.com>
2022-10-11 16:22:45 -0300

    published

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,9 +8,6 @@ module.exports = {
 		extend: {},
 	},
 	plugins: [
-		require('@tailwindcss/aspect-ratio'),
-		require('@tailwindcss/forms'),
-		require('@tailwindcss/line-clamp'),
 		require('@tailwindcss/typography'),
 	],
 };
